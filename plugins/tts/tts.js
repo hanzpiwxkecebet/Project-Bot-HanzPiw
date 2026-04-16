349cdf1101847bdfeb7e21ffb6651e4b749a6685
HanzPiw Official <antonisuhendra59@gmail.com>
2026-04-16 18:18:12 +0700

    Add files via upload

---
+++ plugins/tts/tts.js
@@ -0,0 +1,56 @@
+const gTTS = require('gtts')
+const { spawn } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const { f } = require('../../src/lib/ourin-http')
+const te = require('../../src/lib/ourin-error')
+
+const pluginConfig = {
+    name: 'tts',
+    alias: ['say'],
+    category: 'tts',
+    description: 'Google Text To Speech',
+    usage: '.tts <text>',
+    example: '.tts halo semua',
+    cooldown: 10,
+    energi: 1,
+    isEnabled: true
+}
+
+async function handler(m, { sock }) {
+    const text = m.text?.trim()
+
+    if (!text) {
+        return m.reply(`🎤 *Google TTS*\n\nGunakan:\n${m.prefix}tts halo dunia`)
+    }
+
+    m.react('🎤')
+
+    async function textToSpeech2(text) {
+  try {
+    const response = await f(`https://api.nexray.web.id/ai/gemini-tts?text=${encodeURIComponent(text)}`)
+    return response
+  } catch (error) {
+    return error
+  }
+}
+
+    try {
+
+        const t = await textToSpeech2(text)
+        await sock.sendMessage(m.chat, {
+            audio: { url: t.result },
+            mimetype: 'audio/mpeg',
+        }, { quoted: m })
+        m.react('✅')
+
+    } catch (err) {
+        m.react('☢')
+        m.reply(te(m.prefix, m.command, m.pushName))
+    }
+}
+
+module.exports = {
+    config: pluginConfig,
+    handler
+}
